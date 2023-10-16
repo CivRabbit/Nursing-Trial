57c302291fa590f528ecb14fec53d6d6fd52dae6
CivRabbit <127736021+CivRabbit@users.noreply.github.com>
2023-10-16 11:31:45 +0100

    interactions

---
+++ story_content/user.js
@@ -0,0 +1,499 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6CpBCIeRHTl":
+        Script1();
+        break;
+      case "5zLZVciCRwt":
+        Script2();
+        break;
+      case "5duAkDFY97d":
+        Script3();
+        break;
+      case "6aUgw1rSDn3":
+        Script4();
+        break;
+      case "5cktPborK6L":
+        Script5();
+        break;
+      case "6AD0ljx9XZM":
+        Script6();
+        break;
+      case "5bZTXV89hlm":
+        Script7();
+        break;
+      case "6S5UMPyCfDN":
+        Script8();
+        break;
+      case "5em3tXGMeBe":
+        Script9();
+        break;
+      case "6a0CWUkUgl5":
+        Script10();
+        break;
+      case "6kv39fYEJq7":
+        Script11();
+        break;
+      case "69BX4fSwRVt":
+        Script12();
+        break;
+      case "5Zp2XdmrSkp":
+        Script13();
+        break;
+      case "5wMcO1UnP4J":
+        Script14();
+        break;
+      case "5rWlXO8ERtJ":
+        Script15();
+        break;
+      case "6ibVThJfmtC":
+        Script16();
+        break;
+      case "5foeF63JvOU":
+        Script17();
+        break;
+      case "6f1N5RuC0pu":
+        Script18();
+        break;
+      case "67HLftKugfM":
+        Script19();
+        break;
+      case "60zWcLTmlke":
+        Script20();
+        break;
+      case "5cWaEdVyRl2":
+        Script21();
+        break;
+      case "6LdmaofjIaj":
+        Script22();
+        break;
+      case "65CrKtJF0CN":
+        Script23();
+        break;
+      case "69vLA3nbXUX":
+        Script24();
+        break;
+      case "5uze6ZNjRZl":
+        Script25();
+        break;
+      case "5ylY3nNzzf8":
+        Script26();
+        break;
+      case "6aeioyyeLhK":
+        Script27();
+        break;
+      case "6qQdW9F5aGF":
+        Script28();
+        break;
+      case "6geSnwRLbup":
+        Script29();
+        break;
+      case "5el8xJnHEH5":
+        Script30();
+        break;
+      case "6Pn9sKSYzz7":
+        Script31();
+        break;
+      case "6iU3c303eIY":
+        Script32();
+        break;
+      case "67jTFBgYA8k":
+        Script33();
+        break;
+      case "6RrwrwCkig1":
+        Script34();
+        break;
+      case "5wHQagMaiXf":
+        Script35();
+        break;
+      case "6dfOsI0P4Lc":
+        Script36();
+        break;
+      case "6UFZeoVcNLe":
+        Script37();
+        break;
+      case "6Jd4EyclG0l":
+        Script38();
+        break;
+      case "6dlfSomUf1M":
+        Script39();
+        break;
+      case "5egKzXN55dy":
+        Script40();
+        break;
+      case "66JtEGaZW6B":
+        Script41();
+        break;
+      case "6mpOOci8cii":
+        Script42();
+        break;
+      case "6XM2PzoFuEj":
+        Script43();
+        break;
+      case "5vuxbT89aOT":
+        Script44();
+        break;
+      case "6esixZYqKyx":
+        Script45();
+        break;
+      case "5fkjjQOLP0Z":
+        Script46();
+        break;
+      case "5fnxYsAYN2x":
+        Script47();
+        break;
+      case "5x0o6DhOXnO":
+        Script48();
+        break;
+      case "6m5enO738vj":
+        Script49();
+        break;
+      case "5h0EOq6hLt2":
+        Script50();
+        break;
+      case "6lJU7TlBUKK":
+        Script51();
+        break;
+      case "6DC8G2w6aab":
+        Script52();
+        break;
+      case "6lpuBx1xwrD":
+        Script53();
+        break;
+      case "6fEQsOfhyZ2":
+        Script54();
+        break;
+      case "6d3BICq5ERb":
+        Script55();
+        break;
+      case "6fZzhvAMgTI":
+        Script56();
+        break;
+      case "6c8sZOhAW4z":
+        Script57();
+        break;
+      case "5seJ0E5aveu":
+        Script58();
+        break;
+      case "6kkoUFufau4":
+        Script59();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script2()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script3()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script4()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script5()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script6()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script7()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script8()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script9()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script10()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script11()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script12()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script13()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script14()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script15()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script16()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script17()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script18()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script19()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script20()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script21()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script22()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script23()
+{
+  var player = GetPlayer(); 
+var targetNumber = player.GetVar("scorePercent");
+var baseNumber = Math.floor(targetNumber); 
+var upOrdown = targetNumber - baseNumber
+if (upOrdown >= .5) {
+   targetNumber = baseNumber +1
+   } else{
+   targetNumber = baseNumber
+   }; 
+player.SetVar("scorePercent", targetNumber); 
+}
+
+function Script24()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script25()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script26()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script27()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script28()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script29()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script30()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script31()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script32()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script33()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script34()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script35()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script36()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script37()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script38()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script39()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script40()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script41()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script42()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script43()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script44()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script45()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script46()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script47()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script48()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script49()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script50()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script51()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script52()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script53()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script54()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script55()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script56()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script57()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script58()
+{
+  window.checkSecurity ? checkSecurity() : GetPlayer().SetVar("security_status", "1");
+}
+
+function Script59()
+{
+  var player = GetPlayer(); 
+var targetNumber = player.GetVar("scorePercent");
+var baseNumber = Math.floor(targetNumber); 
+var upOrdown = targetNumber - baseNumber
+if (upOrdown >= .5) {
+   targetNumber = baseNumber +1
+   } else{
+   targetNumber = baseNumber
+   }; 
+player.SetVar("scorePercent", targetNumber); 
+
+if (window.blcSendScore != undefined) window.blcSendScore();
+}
+
